9fda720db0afa025595ba5efa57acc362fcb289d
leviisamuel <levi.samuel@btech.christuniversity.in>
2024-11-14 09:50:08 +0530

    uploaded the src folder

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import "./Carousel.css";
+import { images } from "../data/CarouselData";
+
+// you can research - how to use material ui
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+// akjdnkjdfdkjsdvisvdasvbadshbv
+// complete the function below:
+function Carousel() {
+
+    return <h1>Hello world from crousel</h1>
+
+}
+
+export default Carousel;
